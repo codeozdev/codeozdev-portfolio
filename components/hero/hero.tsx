6f4718911f10f1e2b9b820b9c7263e92cbc7a939
ozudev <ozudev@gmail.com>
2024-07-02 03:48:17 +0300

    update link

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,66 +1,66 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { BsGithub, BsLinkedin, BsTwitterX } from 'react-icons/bs'
-import AnimationTextComponent from './animate-text'
-import EvervaultImage from './evervault-image'
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub, BsLinkedin, BsTwitterX } from "react-icons/bs";
+import AnimationTextComponent from "./animate-text";
+import EvervaultImage from "./evervault-image";
 
 export default function HeroComponent() {
   return (
-    <div className='relative mt-5 h-full w-full md:mt-10'>
-      <div className='relative z-20'>
+    <div className="relative mt-5 h-full w-full md:mt-10">
+      <div className="relative z-20">
         {/* GRID */}
-        <div className='container grid grid-cols-1 place-items-center gap-10 p-8 md:grid-cols-2 md:gap-0 md:p-0'>
-          <div className='flex h-fit w-full flex-col items-center justify-center space-y-3 text-center md:items-start md:justify-start md:space-y-5 md:text-start'>
+        <div className="container grid grid-cols-1 place-items-center gap-10 p-8 md:grid-cols-2 md:gap-0 md:p-0">
+          <div className="flex h-fit w-full flex-col items-center justify-center space-y-3 text-center md:items-start md:justify-start md:space-y-5 md:text-start">
             {/*MOBILE IMAGE*/}
             <Image
-              src='/photo.png'
-              alt='img'
+              src="/photo.png"
+              alt="img"
               width={500}
               height={500}
               className={`mb-5 max-h-40 w-full max-w-40 rounded-full object-cover object-top grayscale hover:grayscale-0 dark:border dark:border-primary dark:bg-[#140C1C] sm:mb-0 md:hidden`}
-              sizes='60vw'
+              sizes="60vw"
             />
-            <div className='font-sora text-xl md:text-2xl 3xl:text-3xl'>
+            <div className="font-sora text-xl md:text-2xl 3xl:text-3xl">
               I{`'`}m Oğuzhan /
               <AnimationTextComponent />
             </div>
-            <h1 className='inline-block max-w-[300px] bg-gradient-to-r from-primary to-white bg-clip-text text-3xl font-bold leading-tight tracking-wider text-transparent md:max-w-[600px] md:text-6xl 3xl:max-w-[800px] 3xl:text-7xl'>
+            <h1 className="inline-block max-w-[300px] bg-gradient-to-r from-primary to-white bg-clip-text text-3xl font-bold leading-tight tracking-wider text-transparent md:max-w-[600px] md:text-6xl 3xl:max-w-[800px] 3xl:text-7xl">
               Web Developer + UX Designer
             </h1>
-            <p className='text-center font-sora text-sm tracking-wide md:max-w-[600px] md:text-start  md:text-base 3xl:max-w-[800px] 3xl:text-lg'>
+            <p className="text-center font-sora text-sm tracking-wide md:max-w-[600px] md:text-start  md:text-base 3xl:max-w-[800px] 3xl:text-lg">
               I have been working in both frontend and backend development for 1.5 years, and I am
               passionate about keeping up with industry inn ovations and adapting to new
               technologies. I am confident in my ability to solve problems and produce efficient
               solutions.
             </p>
-            <ul className='flex items-center justify-center gap-5 md:justify-start'>
-              <li className='cursor-pointer rounded-full border border-primary fill-primary p-2 duration-500 ease-in-out hover:scale-110'>
-                <Link href='#'>
+            <ul className="flex items-center justify-center gap-5 md:justify-start">
+              <li className="cursor-pointer rounded-full border border-primary fill-primary p-2 duration-500 ease-in-out hover:scale-110">
+                <Link href="https://x.com/codeozdev" target="_blank">
                   <BsTwitterX />
                 </Link>
               </li>
-              <li className='cursor-pointer rounded-full border  border-primary fill-primary p-2 duration-500 ease-in-out hover:scale-110'>
-                <Link href='#'>
+              <li className="cursor-pointer rounded-full border  border-primary fill-primary p-2 duration-500 ease-in-out hover:scale-110">
+                <Link href="https://www.linkedin.com/in/oguzhanuguz" target="_blank">
                   <BsLinkedin />
                 </Link>
               </li>
-              <li className='cursor-pointer rounded-full border  border-primary fill-primary p-2 duration-500 ease-in-out hover:scale-110'>
-                <Link href='#'>
+              <li className="cursor-pointer rounded-full border  border-primary fill-primary p-2 duration-500 ease-in-out hover:scale-110">
+                <Link href="https://github.com/codeozdev" target="_blank">
                   <BsGithub />
                 </Link>
               </li>
             </ul>
           </div>
-          <div className='absolute hidden justify-self-end md:block'>
+          <div className="absolute hidden justify-self-end md:block">
             <EvervaultImage />
           </div>
         </div>
       </div>
       {/*overlay*/}
       <div className="absolute inset-0 h-full w-full dark:animate-pulse dark:bg-[url('/Untitled.jpg')] md:bg-left" />
-      <div className='dark-gradient inset-0 h-full w-full dark:absolute' />
+      <div className="dark-gradient inset-0 h-full w-full dark:absolute" />
     </div>
-  )
+  );
 }
 
 //dark:absolute  vermemizin sebebi dark-gradient custom css'e dark mod olarak atayamadigimizdan dolayi
--- components/project/project-list.tsx
+++ components/project/project-list.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
-import { projects } from '@/types/projects'
-import Image from 'next/image'
-import Link from 'next/link'
-import { MdArrowOutward } from 'react-icons/md'
-import { Tabs } from '../ui/tabs'
+import { projects } from "@/types/projects";
+import Image from "next/image";
+import Link from "next/link";
+import { MdArrowOutward } from "react-icons/md";
+import { Tabs } from "../ui/tabs";
 
 export function TabsDemo() {
   const tabs: any = projects.map((project) => {
@@ -14,22 +14,22 @@ export function TabsDemo() {
       content: (
         <div
           key={project.id}
-          className='relative h-full w-full overflow-hidden md:rounded-2xl bg-gradient-to-b from-[#E5E7EB] to-slate-200 dark:from-[#0D0D0F] dark:to-black p-10 flex flex-col gap-5 items-center main'>
-          <div className='flex items-center justify-between w-full'>
-            <div className='hidden md:block' />
-            <h1 className='text-xl font-bold md:text-4xl'>{project.title} Tab</h1>
-            <Link href={project.link} target='_blank' className=''>
+          className="relative h-full w-full overflow-hidden md:rounded-2xl bg-gradient-to-b from-[#E5E7EB] to-slate-200 dark:from-[#0D0D0F] dark:to-black p-10 flex flex-col gap-5 items-center main">
+          <div className="flex items-center justify-between w-full">
+            <div className="hidden md:block" />
+            <h1 className="text-xl font-bold md:text-4xl">{project.title} Tab</h1>
+            <Link href={project.link} target="_blank" className="">
               <MdArrowOutward
                 size={30}
-                className='rotate-element  fill-[#8F5DF7] dark:fill-white'
+                className="rotate-element  fill-[#8F5DF7] dark:fill-white"
               />
             </Link>
           </div>
-          <div className='md:flex gap-2 hidden'>
+          <div className="md:flex gap-2 hidden">
             {project.tools.map((tool) => (
               <div
                 key={tool}
-                className='cursor-default rounded-full border-2 border-black/40 px-4 py-2 text-xs transition hover:border-black hover:text-black dark:border-white/20 dark:hover:border-white dark:hover:text-white'>
+                className="cursor-default rounded-full border-2 border-black/40 px-4 py-2 text-xs transition hover:border-black hover:text-black dark:border-white/20 dark:hover:border-white dark:hover:text-white">
                 {tool}
               </div>
             ))}
@@ -37,26 +37,26 @@ export function TabsDemo() {
           <DummyContent image={project.image} />
         </div>
       ),
-    }
-  })
+    };
+  });
 
   return (
     <div
-      className='b relative mx-auto flex h-[20rem] w-full max-w-5xl flex-col items-start justify-start [perspective:1000px] md:h-[40rem] md:mt-[3.5rem] mb-10 md:mb-20'
-      id='projects'>
+      className="b relative mx-auto flex h-[20rem] w-full max-w-5xl flex-col items-start justify-start [perspective:1000px] md:h-[40rem] md:mt-[3.5rem] mb-10 md:mb-20"
+      id="projects">
       <Tabs tabs={tabs} />
     </div>
-  )
+  );
 }
 
 const DummyContent = ({ image }: any) => {
   return (
     <Image
       src={image}
-      alt='dummy image'
-      width='1000'
-      height='1000'
-      className='absolute inset-x-0 -bottom-10 w-full md:mx-auto h-[80%] md:w-[90%] md:rounded-xl object-cover object-left-top'
+      alt="dummy image"
+      width="1000"
+      height="1000"
+      className="absolute inset-x-0 -bottom-10 w-full md:mx-auto h-[80%] md:w-[90%] md:rounded-xl object-cover object-left-top"
     />
-  )
-}
+  );
+};
